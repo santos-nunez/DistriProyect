66b84d97b1c0e710e8e06ebc95a3c1e5239d2e85
AntonioRiveraDelgado <gustavorivera@javeriana.edu.co>
2021-04-01 21:33:47 -0500

    Project1.5
    Se agrego una cola en el gestor para recibir los mensajes

--- ProyectoBibliotecaGestorCarga/src/main/java/com/javeriana/edu/co/GestorCarga.java
+++ ProyectoBibliotecaGestorCarga/src/main/java/com/javeriana/edu/co/GestorCarga.java
@@ -4,6 +4,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import javax.sound.sampled.SourceDataLine;
 
@@ -31,65 +33,67 @@ public class GestorCarga {
             Hilo h = new Hilo("", publisher, "");
             Hilo h2 = new Hilo("", publisher, "");
 
+            Queue<String> cola = new LinkedList();
             while (!Thread.currentThread().isInterrupted()) {
                 /**
                  * Conectar con cliente
                  */
-                tipoSolicitud = "NINGUNA";
                 byte[] reply = socket.recv(0);
                 System.out.println("Received " + ": [" + new String(reply, ZMQ.CHARSET) + "]");
                 mensaje = new String(reply, ZMQ.CHARSET);
-                mensajes = mensaje.split(" ");
-                if (mensajes.length == 2) {
-                    tipoSolicitud = mensajes[0];
-                    id = Integer.valueOf(mensajes[1]);
+                cola.add(mensaje);
 
-                }
                 if (tipoSolicitud != "SOLICITAR") {
                     String response = "PETICION DE " + tipoSolicitud + " ACEPTADA";
                     socket.send(response.getBytes(ZMQ.CHARSET), 0);
                 }
+                while (cola.size() > 0) {
+                    /**
+                     * Conectar con Actor
+                     */
+                    tipoSolicitud = "NINGUNA";
+                    String enCola = cola.peek();
+                    mensajes = enCola.split(" ");
+                    if (mensajes.length == 2) {
+                        tipoSolicitud = mensajes[0];
+                        id = Integer.valueOf(mensajes[1]);
+                    }
+                    if (tipoSolicitud.equals("RENOVAR") && !h.isAlive()) {
 
-                /**
-                 * Conectar con Actor
-                 */
-                if (tipoSolicitud.equals("RENOVAR") && !h.isAlive()) {
+                        int codigoTopico = 10000;
 
-                    int codigoTopico = 10000;
+                        Date dateS = new Date(), dateF = new Date();
+                        Calendar cal = Calendar.getInstance();
+                        cal.setTime(dateS);
+                        cal.add(Calendar.DATE, 7);
+                        dateF = cal.getTime();
 
-                    Date dateS = new Date(), dateF = new Date();
-                    Calendar cal = Calendar.getInstance();
-                    cal.setTime(dateS);
-                    cal.add(Calendar.DATE, 7);
-                    dateF = cal.getTime();
+                        int year = 1900 + dateS.getYear(), year2 = 1900 + dateF.getYear();
+                        int mes = dateS.getMonth() + 1, mes2 = dateF.getMonth() + 1;
 
-                    int year = 1900 + dateS.getYear(), year2 = 1900 + dateF.getYear();
-                    int mes = dateS.getMonth() + 1, mes2 = dateF.getMonth() + 1;
+                        String fecha = dateS.getDate() + "-" + mes + "-" + year;
+                        String fecha2 = dateF.getDate() + "-" + mes2 + "-" + year2;
+                        String enviar = fecha + " " + fecha2;
+                        String update = String.format("%d %d %s", codigoTopico, id, enviar);
+                        h = new Hilo("renovar", publisher, update);
+                        h.start();
+                        cola.poll();
 
-                    String fecha = dateS.getDate() + "-" + mes + "-" + year;
-                    String fecha2 = dateF.getDate() + "-" + mes2 + "-" + year2;
-                    String enviar = fecha + " " + fecha2;
-                    String update = String.format("%d %d %s", codigoTopico, id, enviar);
-                    h = new Hilo("renovar", publisher, update);
-                    h.start();
+                    } else if (tipoSolicitud.equals("DEVOLVER") && !h2.isAlive()) {
 
-                } else if (tipoSolicitud.equals("DEVOLVER") && !h2.isAlive()) {
-
-                    int codigoTopico = 10001;
-                    String update = String.format("%d %d", codigoTopico, id);
-                    h2 = new Hilo("devolver", publisher, update);
-                    h2.start();
-
-                } else if (tipoSolicitud.equals("SOLICITAR") && false) {
-                    int codigoTopico = 10002;
-                    String enviar = arg1;
-                    String update = String.format("%d %d %s", codigoTopico, id, enviar);
-                    h = new Hilo("solicitar", publisher, update);
-                    h.start();
-                } else {
-                    System.out.println("ESPERANDO...");
+                        int codigoTopico = 10001;
+                        String update = String.format("%d %d", codigoTopico, id);
+                        h2 = new Hilo("devolver", publisher, update);
+                        h2.start();
+                        cola.poll();
+                    } else if (tipoSolicitud.equals("SOLICITAR") && false) {
+                        int codigoTopico = 10002;
+                        String enviar = arg1;
+                        String update = String.format("%d %d %s", codigoTopico, id, enviar);
+                        h = new Hilo("solicitar", publisher, update);
+                        h.start();
+                    }
                 }
-                Thread.sleep(200);
 
             }
 
